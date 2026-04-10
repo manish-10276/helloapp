3670b36ad68273a07feb1c42d8eda6b4cb9b18c8
manish-10276 <saimanish312@gmail.com>
2026-04-10 13:20:00 +0530

    coded UC5

--- HelloApp.java
+++ HelloApp.java
@@ -1,14 +1,26 @@
 public class HelloApp {
-    
+
     public static void main(String[] args) {
 
-
-        String name = "World";
-
-        if (args.length > 0) {
-            name = args[0];
+        
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
 
-        System.out.println("Hello, " + name + "!");
+        
+        StringBuilder nameBuilder = new StringBuilder();
+        
+        boolean first = true;
+
+        for (String name : args) {
+            if (!first) {
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(name);
+            first = false;
+        }
+
+        System.out.println("Hello, " + nameBuilder.toString() + "!");
     }
 }
